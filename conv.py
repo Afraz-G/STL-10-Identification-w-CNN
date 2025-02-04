381485ff19a3cb41ffdd852069b78be89f947f82
Afraz-G <155395469+Afraz-G@users.noreply.github.com>
2025-02-04 11:33:39 +1100

    Update conv.py to accommodate for RGB values
    Alterations were needed to be made to allow for a HWC format to properly process the colors as well

--- conv.py
+++ conv.py
@@ -5,10 +5,10 @@ class Conv3x3:
     def __init__(self, num_filters):
         self.num_filters = num_filters
     
-        self.filters = np.random.randn(num_filters, 3, 3) / 9
+        self.filters = np.random.randn(num_filters, 3, 3, 3) / 9
 
     def iterate_regions(self, image):
-        h, w = image.shape
+        h, w, _ = image.shape
 
         for i in range(h-2):
             for j in range(w-2):
@@ -16,10 +16,9 @@ class Conv3x3:
                 yield im_region, i, j
     
     def forward(self, input):
-        h,w = input.shape
+        h,w, _ = input.shape
         output = np.zeros((h-2, w-2, self.num_filters))
-
         for im_region, i, j in self.iterate_regions(input):
-            output[i,j] = np.sum(im_region * self.filters, axis=(1,2))
+            output[i,j] = np.sum(im_region * self.filters, axis=(1,2,3))
         
         return output
