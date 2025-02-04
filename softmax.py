614cced339d257231d953f3b0b8420e9ca80296e
Afraz-G <155395469+Afraz-G@users.noreply.github.com>
2025-02-04 11:37:24 +1100

    Update softmax.py to prevent overflow

--- softmax.py
+++ softmax.py
@@ -3,12 +3,13 @@ import numpy as np
 class Softmax:
 
     def __init__(self,input_len, nodes):
-        self.weights = np.random.randn(input_len, nodes) /  input_len
+        self.input_len = input_len 
+        self.nodes = nodes 
+        self.weights = np.random.randn(input_len, nodes) / input_len
         self.biases = np.zeros(nodes)
 
     def forward(self,input):
         input = input.flatten()
-        input_len, nodes = self.weights.shape
         totals = np.dot(input, self.weights) + self.biases
-        exp = np.exp(totals)
-        return exp / np.sum(exp, axis = 0)
+        exp = np.exp(totals - np.max(totals)) 
+        return exp / np.sum(exp, axis=0)
